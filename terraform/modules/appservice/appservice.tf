3d3c0ec8ccf956113859fe61efd337a9fa9ba8ab
HieuVV <voxvanhieu@gmail.com>
2023-01-03 04:30:52 +0700

    Change az plan + add jmetter

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -3,7 +3,7 @@ resource "azurerm_service_plan" "test" {
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
   os_type             = "Linux"
-  sku_name            = "F1"
+  sku_name            = "B1"
 }
 
 resource "azurerm_linux_web_app" "test" {
