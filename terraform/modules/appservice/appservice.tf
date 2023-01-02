ca46739ad69fdb2a323c5965a724efc9dbb42563
HieuVV <voxvanhieu@gmail.com>
2023-01-03 05:00:05 +0700

    Fix app plan

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -2,10 +2,8 @@ resource "azurerm_service_plan" "test" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
-  sku {
-    tier = "Free"
-    size = "F1"
-  }
+  os_type             = "Windows"
+  sku_name            = "F1"
 }
 
 resource "azurerm_linux_web_app" "test" {
